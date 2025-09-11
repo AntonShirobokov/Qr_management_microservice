002f0b65b0ff0aa84b5f6fb5fa2f6791b9f1f31f
AntonShirobokov <shirobokov.anton2016@gmail.com>
2025-09-11 15:12:09 +0400

    Init commit

---
+++ src/main/java/com/shirobokov/qr_management_microservice/QrManagementMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.shirobokov.qr_management_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrManagementMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrManagementMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shirobokov/qr_management_microservice/QrManagementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shirobokov.qr_management_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrManagementMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
